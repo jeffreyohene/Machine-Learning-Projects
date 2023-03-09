c3a35e2c6b82812e8fb5b1a4f87cff01163f88ab
jeff-ohene <jeff.ohene10@gmail.com>
2023-03-09 20:48:53 +0000

    skript

---
+++ Image Classification/Distinguishing between Bee Species/bee.py
@@ -0,0 +1,182 @@
+# Used to change filepaths
+from pathlib import Path
+
+# We set up matplotlib, pandas, and the display function
+%matplotlib inline
+import matplotlib.pyplot as plt
+from IPython.display import display
+import pandas as pd
+
+# import numpy to use in this cell
+import numpy as np
+
+# import Image from PIL so we can use it later
+from PIL import Image
+
+# generate test_data
+test_data = np.random.beta(1, 1, size=(100, 100, 3))
+
+# display the test_data
+plt.imshow(test_data)
+
+
+## 2. Using PIL to open images
+# open the image
+img = Image.open('datasets/bee_1.jpg')
+
+# Get the image size
+img_size = img.size
+
+print("The image size is: {}".format(img_size))
+
+# Just having the image as the last line in the cell will display it in the notebook
+img
+
+##3. Image manipulation
+# Crop the image to 25, 25, 75, 75
+img_cropped = img.crop([25, 25, 75, 75])
+display(img_cropped)
+
+# rotate the image by 45 degrees
+img_rotated = img.rotate(45, expand=25)
+display(img_rotated)
+
+# flip the image left to right
+img_flipped = img.transpose(Image.FLIP_LEFT_RIGHT)
+display(img_flipped)
+
+## 4. Image color as data arrays
+# Turn our image object into a NumPy array
+img_data = np.array(img)
+
+# get the shape of the resulting array
+img_data_shape = img_data.shape
+
+print("Our NumPy array has the shape: {}".format(img_data_shape))
+
+# plot the data with `imshow`
+plt.imshow(img_data)
+plt.show()
+
+# plot the red channel
+plt.imshow(img_data[:, :, 0], cmap=plt.cm.Reds_r)
+plt.show()
+
+# plot the green channel
+plt.imshow(img_data[:, :, 1], cmap=plt.cm.Greens_r)
+plt.show()
+
+# plot the blue channel
+plt.imshow(img_data[:, :, 2], cmap=plt.cm.Blues_r)
+plt.show()
+
+## 5. Color channels
+def plot_kde(channel, color):
+    """ Plots a kernel density estimate for the given data.
+        
+        `channel` must be a 2d array
+        `color` must be a color string, e.g. 'r', 'g', or 'b'
+    """
+    data = channel.flatten()
+    return pd.Series(data).plot.density(c=color)
+
+# create the list of channels
+channels = ['r', 'g', 'b']
+    
+def plot_rgb(image_data):
+    # use enumerate to loop over colors and indexes
+    for ix, color in enumerate(channels):
+        plot_kde(image_data[:, :, ix], color)
+        
+    plt.show()
+
+plot_rgb(img_data)
+
+## 6. Honey bees v bumble bees: Round 1
+# load bee_12.jpg as honey
+honey = Image.open('datasets/bee_12.jpg')
+
+# display the honey bee image
+display(honey)
+
+# NumPy array of the honey bee image data
+honey_data = np.array(honey)
+
+# plot the rgb densities for the honey bee image
+plot_rgb(honey_data)
+
+## 7. Honey bees v Bumble bees: Round 2
+# load bee_3.jpg as bumble
+bumble = Image.open('datasets/bee_3.jpg')
+
+# display the bumble bee image
+display(bumble)
+
+# NumPy array of the bumble bee image data
+bumble_data = np.array(bumble)
+
+# plot the rgb densities for the bumble bee image
+plot_rgb(bumble_data)
+
+## 8. Simplifying image
+# convert to grayscale
+honey_bw = honey.convert("L")
+display(honey_bw)
+
+# convert the image to a NumPy array
+honey_bw_arr = np.array(honey_bw)
+
+# get the shape of the resulting array
+honey_bw_arr_shape = honey_bw_arr.shape
+print("Our NumPy array has the shape: {}".format(honey_bw_arr_shape))
+
+# plot the array using matplotlib
+plt.imshow(honey_bw_arr, cmap=plt.cm.gray)
+plt.show()
+
+# plot the kde of the new black and white array
+plot_kde(honey_bw_arr, 'k')
+
+## 9. Saving work
+# flip the image left-right with transpose
+honey_bw_flip = honey_bw.transpose(Image.FLIP_LEFT_RIGHT)
+
+# show the flipped image
+display(honey_bw_flip)
+
+# save the flipped image
+honey_bw_flip.save("saved_images/bw_flipped.jpg")
+
+# create higher contrast by reducing range
+honey_hc_arr = np.maximum(honey_bw_arr, 100)
+
+# show the higher contrast version
+plt.imshow(honey_hc_arr, cmap=plt.cm.gray)
+
+# convert the NumPy array of high contrast to an Image
+honey_bw_hc = Image.fromarray(honey_hc_arr)
+
+# save the high contrast version
+honey_bw_hc.save("saved_images/bw_hc.jpg")
+
+## 10. Constructing a pipeline
+image_paths = ['datasets/bee_1.jpg', 'datasets/bee_12.jpg', 'datasets/bee_2.jpg', 'datasets/bee_3.jpg']
+
+def process_image(path):
+    img = Image.open(path)
+
+    # create paths to save files to
+    bw_path = "saved_images/bw_{}.jpg".format(path.stem)
+    rcz_path = "saved_images/rcz_{}.jpg".format(path.stem)
+
+    print("Creating grayscale version of {} and saving to {}.".format(path, bw_path))
+    bw = img.convert("L")
+    bw.save(bw_path)
+    
+    print("Creating rotated, cropped, and zoomed version of {} and saving to {}.".format(path, rcz_path))
+    rcz = bw.rotate(45).crop([25, 25, 75, 75]).resize((100, 100))
+    rcz.save(rcz_path)
+
+# for loop over image paths
+for img_path in image_paths:
+    process_image(Path(img_path))
